d307ecfcd2765d8dee056cf427b7d5bd2aae27f5
Sam Enoka <slwenoka@gmail.com>
2016-02-20 10:58:14 +1000

    Remove semi-colons from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
-var fs           = require('fs');
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var ghPages      = require('gulp-gh-pages');
-var gulpif       = require('gulp-if');
-var source       = require('vinyl-source-stream');
-var buffer       = require('vinyl-buffer');
-var browserify   = require('browserify');
-var watchify     = require('watchify');
-var babelify     = require('babelify');
-var uglify       = require('gulp-uglify');
-var eslint       = require('gulp-eslint');
-var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var rimraf       = require('rimraf');
-var assign       = require('lodash/assign');
-var browserSync  = require('browser-sync');
-var imagemin     = require('gulp-imagemin');
+var fs           = require('fs')
+var gulp         = require('gulp')
+var gutil        = require('gulp-util')
+var ghPages      = require('gulp-gh-pages')
+var gulpif       = require('gulp-if')
+var source       = require('vinyl-source-stream')
+var buffer       = require('vinyl-buffer')
+var browserify   = require('browserify')
+var watchify     = require('watchify')
+var babelify     = require('babelify')
+var uglify       = require('gulp-uglify')
+var eslint       = require('gulp-eslint')
+var sass         = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
+var rimraf       = require('rimraf')
+var assign       = require('lodash/assign')
+var browserSync  = require('browser-sync')
+var imagemin     = require('gulp-imagemin')
 
 var config = {
   production: !!gutil.env.production,
@@ -32,21 +32,21 @@ var config = {
     open: false,
     notify: false
   }
-};
+}
 
-process.env.NODE_ENV = config.production ? 'production' : 'development';
+process.env.NODE_ENV = config.production ? 'production' : 'development'
 
-gulp.task('default', ['lint', 'js', 'scss', 'images', 'html']);
+gulp.task('default', ['lint', 'js', 'scss', 'images', 'html'])
 
 gulp.task('clean', function(cb){
-  rimraf(config.dest, cb);
-});
+  rimraf(config.dest, cb)
+})
 
 gulp.task('lint', function () {
   return gulp.src(['./src/**/*.js'])
     .pipe(eslint({ useEslintrc: true }))
     .pipe(eslint.format())
-});
+})
 
 /**
  * Browserify and transpile ES6 -> ES5.
@@ -59,30 +59,30 @@ gulp.task('js', ['lint'], function() {
     fullPaths: !config.production,
     cache: {},
     packageCache: {}
-  }).transform(babelify);
+  }).transform(babelify)
 
   var rebundle = function() {
     return bundler.bundle()
       .on('error', function(err) {
-        console.log('Bundle Error: ' + err.message);
+        console.log('Bundle Error: ' + err.message)
       })
       .pipe(source(config.output))
       .pipe(buffer())
       .pipe(gulpif(config.production, uglify()))
       .pipe(gulp.dest(config.dest))
-      .pipe(browserSync.stream());
+      .pipe(browserSync.stream())
   }
 
   if (!config.production) {
-    bundler = watchify(bundler);
+    bundler = watchify(bundler)
     bundler.on('update', function() {
-      gulp.start('lint');
-      rebundle();
-    });
+      gulp.start('lint')
+      rebundle()
+    })
   }
 
-  return rebundle();
-});
+  return rebundle()
+})
 
 gulp.task('scss', function() {
   gulp.src('./src/app.scss')
@@ -91,32 +91,32 @@ gulp.task('scss', function() {
     }).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./public'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('images', function() {
   return gulp.src(config.images)
     .pipe(imagemin())
     .pipe(gulp.dest(config.dest + '/images'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('html', function() {
   return gulp.src(config.html)
-    .pipe(gulp.dest(config.dest));
-});
+    .pipe(gulp.dest(config.dest))
+})
 
 gulp.task('watch', ['js'], function () {
-  gulp.watch(config.scss, ['scss']);
-  gulp.watch(config.images, ['images']);
-  gulp.watch(config.html, ['html']).on('change', browserSync.reload);
-});
+  gulp.watch(config.scss, ['scss'])
+  gulp.watch(config.images, ['images'])
+  gulp.watch(config.html, ['html']).on('change', browserSync.reload)
+})
 
 gulp.task('serve', ['default', 'watch'], function () {
-  browserSync.init(config.browserSync);
-});
+  browserSync.init(config.browserSync)
+})
 
 gulp.task('deploy', function() {
   return gulp.src(config.dest + '**/*')
-    .pipe(ghPages());
-});
+    .pipe(ghPages())
+})
