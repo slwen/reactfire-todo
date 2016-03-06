9f4203edb8c6debb279479a8213272f4b62f2542
Sam Enoka <slwenoka@gmail.com>
2016-03-06 18:58:58 +1000

    Add a test for List component

---
+++ src/components/List/__tests__/List-test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import List from '../'
+import { render } from 'enzyme'
+
+let component
+const mockProps = {
+  items: [
+    { '.key': 'rAnDOMmumBOjuMbO', done: true, text: 'potato' },
+    { '.key': 'fooBArBAZbOOM', done: false, text: 'onion' }
+  ],
+  uid: 'github:000000',
+  clear: sinon.spy(),
+  itemStore: {}
+}
+
+describe('List component', () => {
+  beforeEach(() => {
+    component = render(<List { ...mockProps } />)
+  })
+
+  it('renders a list of todo items', () => {
+    component.find('.Todo').length.should.equal(2)
+  })
+
+  it('renders an `add todo` button', () => {
+    component.find('.AddTodo').length.should.equal(1)
+  })
+
+  describe('completedCounter method', () => {
+    it('count the number of completed items', () => {
+      component = renderIntoDocument(<List { ...mockProps } />)
+      component.completedCounter(mockProps.items).should.equal(1)
+    });
+  });
+})
